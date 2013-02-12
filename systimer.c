4265ee2fb321e6233fa9eac3a9c7ba78f1f3bb07
Blake Bourque <Techplex.Engineer@gmail.com>
2013-02-12 11:50:24 -0500

    The added macros __init and __exit throw away the init and exit licence when the kernel is compiled as a monolithic kernel.

--- systimer.c
+++ systimer.c
@@ -1,16 +1,20 @@
 
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/init.h>
 
 
-int init_module(void)
+static int __init rpisystimer_minit(void)
 {
 	printk(KERN_NOTICE "Systimer Loaded\n");
 	return 0;
 }
 
-void cleanup_module(void)
+static void __exit rpisystimer_mcleanup(void)
 {
 	printk(KERN_NOTICE "Goodbye\n");
 	return;
 }
+
+module_init(rpisystimer_minit);
+module_exit(rpisystimer_mcleanup);
