c70ba2e9df0d59c57a0771a8f6939f7114dc5e14
Blake Bourque <Techplex.Engineer@gmail.com>
2013-02-14 11:26:23 -0500

    Added varargs

--- systimer.c
+++ systimer.c
@@ -2,17 +2,23 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/init.h>
+#include <linux/stat.h>
 
 #define SYSTIMER_MOD_AUTH "Techplex"
 #define SYSTIMER_MOD_DESC "SysTimer for Raspberry Pi"
 #define SYSTIMER_MOD_SDEV "SysTimerRPi" //supported devices
 
 static int sint __initdata = 0;
+static int systimer_mode = 0;
+
+module_param(systimer_mode, int, S_IRUSR|S_IWUSR|S_IRGRP);
+MODULE_PARM_DESC(systimer_mode, "Systimer mode");
 
 static int __init rpisystimer_minit(void)
 {
 	printk(KERN_NOTICE "Systimer Loaded\n");
 	printk(KERN_INFO "Var: %d\n", sint);
+	printk(KERN_INFO "Var2: %d\n", systimer_mode);
 	return 0;
 }
 
