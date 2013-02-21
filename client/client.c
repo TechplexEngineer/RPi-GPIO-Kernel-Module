27fa0b33e288a8d82904682aa4dd6f3d385e63e3
Blake Bourque <Techplex.Engineer@gmail.com>
2013-02-21 11:30:08 -0500

    Client code working

--- client/client.c
+++ client/client.c
@@ -2,35 +2,52 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
+#include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdint.h>
 
 #include "systimer.h"
 
 int main (int argc, char*argv[])
 {
 	int fd;
-	fd = open("/dev/systimer", O_RDWR);
-	if(fd) {
-		perror("open(O_RDWR)");
-	} else {
-		close(fd);
-	}
-
-	fd = open("/dev/systimer", O_WRONLY);
-	if(fd) {
-		perror("open(O_WRONLY)");
-	} else {
-		close(fd);
-	}
-
+	uint64_t t;
+	int ret;
+	
 	fd = open("/dev/systimer", O_RDONLY);
-	if(fd) {
+	if(!fd) {
 		perror("open(O_RDONLY)");
-	} else {
-		close(fd);
+		return errno;
 	}
+
+	ret  = ioctl(fd, SYSTIMER_READ, &t);
+	if (ret < 0) {
+		perror("ioclt");
+		close(fd);
+		return errno;
+	} else {
+		printf("%llu\n",t);
+	}
+	
+	
+/*
+	fd = open("/dev/systimer", O_RDWR);
+    if(fd) {
+        perror("open(O_RDWR)");
+    } else {
+        close(fd);
+    }
+
+    fd = open("/dev/systimer", O_WRONLY);
+    if(fd) {
+        perror("open(O_WRONLY)");
+    } else {
+        close(fd);
+    }
+*/
+	close(fd);
 	return 0;
 }
 
