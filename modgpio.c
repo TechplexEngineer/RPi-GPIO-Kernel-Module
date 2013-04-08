b8d6407891865acf9c7ff15d723da73f3776cf5c
Blake Bourque <blake.bourque@maine.edu>
2013-04-07 21:25:53 -0400

    All ioctl commands seem to be working

--- modgpio.c
+++ modgpio.c
@@ -32,61 +32,53 @@ static long st_ioctl(struct file *filp, unsigned int cmd, unsigned long arg);
 #define PIN_NULL_PID 5	//signifies not a valid pin. Unsigned number
 #define PIN_UNASSN 0	//signifies pin available
 #define PIN_ARRAY_LEN 32
-uint32_t pins[PIN_ARRAY_LEN] = {			//note this is for rpiv2
-	PIN_NULL_PID,
-	PIN_NULL_PID,
-	PIN_UNASSN,	//pin 2
-	PIN_UNASSN,	//pin 3
-	PIN_UNASSN,	//pin 4
-	PIN_NULL_PID,
-	PIN_NULL_PID,
-	PIN_UNASSN,	//pin 7
-	PIN_UNASSN,	//pin 8
-	PIN_UNASSN,	//pin 9
-	PIN_UNASSN,	//pin 10
-	PIN_UNASSN,	//pin 11
-	PIN_NULL_PID,
-	PIN_NULL_PID,
-	PIN_UNASSN,	//pin 14
-	PIN_UNASSN,	//pin 15
-	PIN_NULL_PID,
-	PIN_UNASSN,	//pin 17
-	PIN_UNASSN,	//pin 18
-	PIN_NULL_PID,
-	PIN_NULL_PID,
-	PIN_NULL_PID,
-	PIN_UNASSN,	//pin 22
-	PIN_UNASSN,	//pin 23
-	PIN_UNASSN,	//pin 24
-	PIN_UNASSN,	//pin 25
-	PIN_NULL_PID,
-	PIN_UNASSN,	//pin 27
-	PIN_UNASSN,	//pin 28
-	PIN_UNASSN,	//pin 29
-	PIN_UNASSN,	//pin 30
-	PIN_UNASSN,	//pin 31
-};
-
-// //in init malloc a struct
-// // int pin
-// // int procid
-// // if the ptr is null its not assiginable
-
-// int validPins[] {
-// 	2, 3, 4, 7, 8, 9, 10, 11, 14, 15, 17, 18, 22, 23, 24, 25, 27, 28, 29, 30, 31,
-// }
 
 //Global variables:
 struct gpiomod_data {
 	int mjr;
 	struct class *cls;
 	spinlock_t lock;
+	uint32_t pins[PIN_ARRAY_LEN];
 };
 
 static struct gpiomod_data std = {
 	.mjr = 0,
 	.cls = NULL,
 	//.lock= NULL
+	.pins = {			//note this is for rpiv2
+		PIN_NULL_PID,
+		PIN_NULL_PID,
+		PIN_UNASSN,	//pin 2
+		PIN_UNASSN,	//pin 3
+		PIN_UNASSN,	//pin 4
+		PIN_NULL_PID,
+		PIN_NULL_PID,
+		PIN_UNASSN,	//pin 7
+		PIN_UNASSN,	//pin 8
+		PIN_UNASSN,	//pin 9
+		PIN_UNASSN,	//pin 10
+		PIN_UNASSN,	//pin 11
+		PIN_NULL_PID,
+		PIN_NULL_PID,
+		PIN_UNASSN,	//pin 14
+		PIN_UNASSN,	//pin 15
+		PIN_NULL_PID,
+		PIN_UNASSN,	//pin 17
+		PIN_UNASSN,	//pin 18
+		PIN_NULL_PID,
+		PIN_NULL_PID,
+		PIN_NULL_PID,
+		PIN_UNASSN,	//pin 22
+		PIN_UNASSN,	//pin 23
+		PIN_UNASSN,	//pin 24
+		PIN_UNASSN,	//pin 25
+		PIN_NULL_PID,
+		PIN_UNASSN,	//pin 27
+		PIN_UNASSN,	//pin 28
+		PIN_UNASSN,	//pin 29
+		PIN_UNASSN,	//pin 30
+		PIN_UNASSN,	//pin 31
+	}
 };
 
 //parameter
@@ -118,9 +110,9 @@ static int st_release(struct inode *inode, struct file *filp)
 	int i;
 	//check to see if the releasing process has any pins checked out, and free them
 	for (i=0; i<PIN_ARRAY_LEN; i++) {
-		if (pins[i] == current->pid) {
+		if (std.pins[i] == current->pid) {
 			printk(KERN_DEBUG "[FREE] Pin:%d From:%d\n", i, current->pid);
-			pins[i] = PIN_UNASSN;
+			std.pins[i] = PIN_UNASSN;
 		}
 	}
 	return 0;
@@ -172,10 +164,11 @@ static long st_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 				printk(KERN_DEBUG "[WRITE] Error copying data from userspace\n");
 				return -EFAULT;
 			}
-			if (pins[wdata.pin] != current->pid) { //make sure the process has it checked out
+			if (std.pins[wdata.pin] != current->pid) { //make sure the process has it checked out
 				return -EACCES;	//Permission denied
 			}
 			printk(KERN_INFO "[WRITE] Pin: %d Val:%d\n", wdata.pin, wdata.data);
+			printk(KERN_DEBUG "info: %lX\n", __io_address (GPIO_BASE + GPSET0));
 
 			if (wdata.data == 1)//set
 				writel (1<<wdata.pin,	__io_address (GPIO_BASE + GPSET0));
@@ -187,10 +180,10 @@ static long st_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		case GPIO_REQUEST:
 			//@todo Need some locking here
 			get_user (pin, (int __user *) arg);
-			if (pin > PIN_ARRAY_LEN || pin < 0 || pins[pin] == PIN_NULL_PID) {
+			if (pin > PIN_ARRAY_LEN || pin < 0 || std.pins[pin] == PIN_NULL_PID) {
 				return -EFAULT;	//Bad address
-			} else if (pins[pin] == PIN_UNASSN) {
-				pins[pin] = current->pid;
+			} else if (std.pins[pin] == PIN_UNASSN) {
+				std.pins[pin] = current->pid;
 				printk(KERN_DEBUG "[REQUEST] Pin:%d Assn To:%d\n", pin, current->pid);
 				return 0;
 			}
@@ -198,10 +191,10 @@ static long st_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 
 		case GPIO_FREE:
 			get_user (pin, (int __user *) arg);
-			if (pin > PIN_ARRAY_LEN || pin < 0 || pins[pin] == PIN_NULL_PID) {
+			if (pin > PIN_ARRAY_LEN || pin < 0 || std.pins[pin] == PIN_NULL_PID) {
 				return -EFAULT;	//Bad address
-			} else if (pins[pin] == current->pid) {
-				pins[pin] = PIN_UNASSN;
+			} else if (std.pins[pin] == current->pid) {
+				std.pins[pin] = PIN_UNASSN;
 				printk(KERN_DEBUG "[FREE] Pin:%d From:%d\n", pin, current->pid);
 				return 0;
 			}
@@ -209,45 +202,41 @@ static long st_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
 		case GPIO_TOGGLE:
 			get_user (pin, (int __user *) arg);
 			///check for write perms, else die
-			if (pin > PIN_ARRAY_LEN || pin < 0 || pins[pin] == PIN_NULL_PID) {
+			if (pin > PIN_ARRAY_LEN || pin < 0 || std.pins[pin] == PIN_NULL_PID) {
 				return -EFAULT;	//Bad address
-			} else if (pins[pin] == current->pid) {
+			} else if (std.pins[pin] == current->pid) {
 				val = readl(__io_address (GPIO_BASE + GPLEV0 + (pin/32)*4)); //move to next long if pin/32 ==1
 				flag = val >> (pin%32); 	//right shift by the remainder
 				flag &= 0x01;				//clear upper bits
 				printk(KERN_DEBUG "[TOGGLE] Pin:%d From:%.1d To:%.1d\n", pin, flag, flag?0:1);
 				if (flag != 1)//set
-					writel (1<<pin,	__io_address (GPIO_BASE + GPSET0));
+					writel (1<<pin,	__io_address(GPIO_BASE + GPSET0));
 				else //clear
-					writel (1<<pin,	__io_address (GPIO_BASE + GPCLR0));
+					writel (1<<pin,	__io_address(GPIO_BASE + GPCLR0));
 				put_user(flag?0:1, (uint8_t __user *)arg);
 				return 0;
 			}
 			return -EACCES;	//Permission denied
 		case GPIO_MODE:
+
 			ret = copy_from_user(&mdata, (struct gpio_data_mode __user *)arg, sizeof(struct gpio_data_mode));
 			if (ret != 0) {
 				printk(KERN_DEBUG "[MODE] Error copying data from userspace\n");
 				return -EFAULT;
 			}
-			if (mdata.pin > 31 || mdata.pin < 0 || pins[mdata.pin] == PIN_NULL_PID) {
+			//validate pin
+			if (mdata.pin > 31 || mdata.pin < 0 || std.pins[mdata.pin] == PIN_NULL_PID) {
 				return -EFAULT;	//Bad address
-			} else if (pins[mdata.pin] == current->pid) { //make sure we have access
-				printk(KERN_DEBUG "[MODE] Pin:%d From:%d\n", mdata.pin, current->pid);
-
-				//get the current value
-				val = readl(__io_address (GPIO_BASE + GPLEV0 + (mdata.pin/32)*4 ));
-				//@todo: clear the three bits before writing --done //*(gpio+((g)/10)) &= ~(7<<(((g)%10)*3))
-				writel((~(0x3<<((mdata.pin%10)*3)) & val),	__io_address (GPIO_BASE + (mdata.pin/10)*4));
-				//~(7<<((mdata.pin%10)*3))
-				//@@ Need to bitwise OR here --check
-				if (mdata.data == MODE_OUTPUT)
-					writel((1<<(mdata.pin%10)*3 | val),	__io_address (GPIO_BASE + (mdata.pin/10)*4)); //enable output 0b001
-				//else if (mdata.data == MODE_INPUT)
-					// if we clear the bits above nothing needs doing here
-					//writel(1<<(mdata.pin%10)*3,	__io_address (GPIO_BASE + (mdata.pin/10)*4)); //enable input 0b000
-				else if (mdata.data != MODE_INPUT) //as input is a concequence of clearing the bits above
-					return -EINVAL;	//Invalid argument
+			} else if (std.pins[mdata.pin] == current->pid) { //make sure we have access
+				if(mdata.data == MODE_INPUT) {
+					writel(~(7<<((mdata.pin %10)*3)) & readl(__io_address(GPIO_BASE + (0x04)*(mdata.pin /10))), __io_address(GPIO_BASE + (0x04)*(mdata.pin/10))); // Set as input 
+					printk(KERN_DEBUG "GPIO - Pin %d set as Input\n", mdata.pin);
+				} else if (mdata.data == MODE_OUTPUT) {
+					//printk(KERN_DEBUG "GPIO - Writing %lx to %lx - val: %lx\n", (val & ~(7<<(((gtmp->pin)%10)*3))), __io_address(GPIO_BASE + (0x04)*(pin/10)), val);
+					writel(~(7<<((mdata.pin %10)*3)) & readl(__io_address(GPIO_BASE + (0x04)*(mdata.pin /10))), __io_address(GPIO_BASE + (0x04)*(mdata.pin/10))); // Set as input to bit clear
+					writel(1<<((mdata.pin % 10)*3) | readl(__io_address(GPIO_BASE + (0x04)*(mdata.pin/10))), __io_address(GPIO_BASE + (0x04)*(mdata.pin/10)));    // Set pin as output
+					printk(KERN_DEBUG "GPIO - Pin %d set as Output\n", mdata.pin);
+				}
 				return 0;
 			}
 			return -EACCES;	//Permission denied
